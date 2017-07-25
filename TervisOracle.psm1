aacd684908ea37fbb5c50cb284d96156b0471af3
ChrisMagnuson <hourglasssand@gmail.com>
2017-07-25 08:35:12 -0400

    Initial commit

---
+++ TervisOracle.psm1
@@ -0,0 +1,61 @@
+﻿$OracleAppliationCNAMELeaf = @"
+apexweblogic
+discoverer
+ebsias
+ebsodbee
+infadac
+obiaodbee
+obieeweblogic
+rpias
+rpodbee
+rpweblogic
+soaodbee
+soaweblogic
+"@ -split "`r`n"
+
+$OracleInfrastructureCNAMELeaf = @"
+DataLoadClassic
+OraDBARMT
+RemoteDesktopWebAccess
+"@
+
+$OracleZetaCNAMELeaf = @"
+ebsias
+ebsodbee
+rpias
+rpodbee
+rpweblogic
+"@
+
+$OracleContractorUsedResourcesOutsideOracleSystems = @"
+rdbrocker2012r2
+rdgateway2012r2
+"@
+
+$EnvironmentsWithOracleApplications = "Delta","Epsilon","Production"
+
+function Get-OracleCNAME {
+    $ADDomain = Get-ADDomain
+
+    foreach ($Environment in $EnvironmentsWithOracleApplications) {
+        foreach ($CNAMELeaf in $OracleAppliationCNAMELeaf) {
+            "$CNAMELeaf.$Environment.$($ADDomain.DNSRoot)"
+        }
+    }
+}
+
+function Test-OracleCNAME {
+    $OracleCNAMEs = Get-OracleCNAME
+    foreach ($CNAME in $OralceCNAMEs) {
+        Resolve-DnsName -Name $CNAME -Type CNAME
+    }
+}
+
+function Get-OracleIPAddresses {
+    $OracleCNAMEs = Get-OracleCNAME
+    foreach ($CNAME in $OralceCNAMEs) {
+        Resolve-DnsName -Name $CNAME |
+        Where-Object QueryType -EQ "A" |
+        Select-Object -ExpandProperty IPAddress
+    }
+}
