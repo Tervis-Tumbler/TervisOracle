f7812595049f6d07ffa917f92ac10de018df789d
ChrisMagnuson <hourglasssand@gmail.com>
2017-07-26 13:48:42 -0400

    Refactor to reduce amount of code

--- TervisOracle.psm1
+++ TervisOracle.psm1
@@ -1,4 +1,4 @@
-﻿$OracleAppliationCNAMELeaf = @"
+﻿$OracleAppliationCNAMEHost = @"
 apexweblogic
 discoverer
 ebsias
@@ -13,35 +13,42 @@ soaodbee
 soaweblogic
 "@ -split "`r`n"
 
-$OracleInfrastructureCNAMELeaf = @"
+$OracleInfrastructureCNAMEHost = @"
 DataLoadClassic
 OraDBARMT
 RemoteDesktopWebAccess
-"@
+"@ -split "`r`n"
 
-$OracleZetaCNAMELeaf = @"
+$OracleZetaCNAMEHost = @"
 ebsias
 ebsodbee
 rpias
 rpodbee
 rpweblogic
-"@
+"@ -split "`r`n"
 
 $OracleContractorUsedResourcesOutsideOracleSystems = @"
 rdbrocker2012r2
 rdgateway2012r2
-"@
+INF-RDWebAcc01
+TFS2012
+SharePoint2007
+TrackIT
+INF-DC1
+INF-DC2
+INF-DC3
+"@ -split "`r`n"
 
 $EnvironmentsWithOracleApplications = "Delta","Epsilon","Production"
 
 function Get-OracleCNAME {
-    $ADDomain = Get-ADDomain
 
     foreach ($Environment in $EnvironmentsWithOracleApplications) {
-        foreach ($CNAMELeaf in $OracleAppliationCNAMELeaf) {
-            "$CNAMELeaf.$Environment.$($ADDomain.DNSRoot)"
-        }
+        $OracleAppliationCNAMEHost | Get-TervisDNSName -EnvironmentName $Environment
     }
+
+    $OracleZetaCNAMEHost | Get-TervisDNSName -EnvironmentName Zeta
+    $OracleInfrastructureCNAMEHost | Get-TervisDNSName -EnvironmentName Infrastructure
 }
 
 function Test-OracleCNAME {
@@ -49,6 +56,29 @@ function Test-OracleCNAME {
     foreach ($CNAME in $OralceCNAMEs) {
         Resolve-DnsName -Name $CNAME -Type CNAME
     }
+}
+
+function Get-OracleManagedServiceHostNeedingAccessToDNSARecord {    
+    $OracleContractorUsedResourcesOutsideOracleSystems | Get-TervisDNSName
+}
+
+function Get-TervisDNSName {
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]$Host,
+        $EnvironmentName
+    )
+    begin {
+        if ( -not $Script:ADDomain) { 
+            $Script:ADDomain = Get-ADDomain
+        }
+    }
+    process {
+        "$Host$(if($EnvironmentName){".$EnvironmentName"}).$($Script:ADDomain.DNSRoot)"
+    }
+}
+
+function Get-OracleManagedServiceHostNeedingAccessToDNSName {
+    $OracleCNAME = Get-OracleCNAME
 }
 
 function Get-OracleIPAddresses {
